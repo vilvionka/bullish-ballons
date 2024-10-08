afa8227f4cea7a359778550235ea7191e0d2917f
vilivonka <gromootvod@lenta.ru>
2024-10-08 18:24:12 +0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,20 @@
+$('.main_wrap_box_sliderBallons').slick({
+  dots: false,
+  infinite: false,
+  prevArrow: $('.main_wrap_box_sliderBallons_nav_prev'),
+  nextArrow: $('.main_wrap_box_sliderBallons_nav_next'),
+  speed: 300,
+  slidesToShow: 3,
+ 
+});
+
+
+$('.main_wrap_box_sliderCoin').slick({
+  dots: false,
+  infinite: false,
+  prevArrow: $('.main_wrap_box_sliderCoin_nav_prev'),
+  nextArrow: $('.main_wrap_box_sliderCoin_nav_next'),
+  speed: 300,
+  slidesToShow: 3,
+});
+
