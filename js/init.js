5151137137c16b6fed5ac31cd0d577ef759a72e9
vilivonka <gromootvod@lenta.ru>
2024-10-16 17:16:42 +0300

    faq

--- js/init.js
+++ js/init.js
@@ -18,3 +18,36 @@ $('.main_wrap_box_sliderCoin').slick({
   slidesToShow: 3,
 });
 
+
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var accordionHeader = this.el.find('.faq_accordion_item_header');
+    accordionHeader.on('click', {
+      el: this.el,
+      multiple: this.multiple
+    },
+      this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      //this is the ul.submenuItems
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.faq_accordion_item_body').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+
+  var accordion = new Accordion($('.faq_accordion'), false);
+});
+
