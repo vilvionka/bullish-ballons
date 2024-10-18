fb7b066c1f4e27505971490df1f09b860b5c07fe
vilivonka <gromootvod@lenta.ru>
2024-10-19 00:38:11 +0300

    last pop_up

--- js/main.js
+++ js/main.js
@@ -2,28 +2,49 @@
 
 
 (() => {
-  let coin = document.querySelector('#coin');
-  let crystals = document.querySelector('#crystals');
+  
   let coinMinus = document.querySelector('#coinMinus');
   let crystalsMinus = document.querySelector('#crystalsMinus');
   let coinPlus = document.querySelector('#coinPlus');
   let crystalsPlus = document.querySelector('#crystalsPlus');
+  let popPlus = document.querySelector('.js_pop_up_plus');
+  let popMinus = document.querySelector('.js_pop_up_minus')
 
   coinMinus.addEventListener('click', function () {
-    let numCoin = Number(coin.innerHTML);
-    coin.innerHTML = numCoin - 1;
+    popMinus.classList.add('active');
   })
   coinPlus.addEventListener('click', function () {
-    let numCoin = Number(coin.innerHTML);
-    coin.innerHTML = numCoin + 1;
+    popPlus.classList.add('active');
   })
   crystalsMinus.addEventListener('click', function () {
-    let numcrystals = Number(crystals.innerHTML);
-    crystals.innerHTML = numcrystals - 1;
+    popMinus.classList.add('active');
   })
   crystalsPlus.addEventListener('click', function () {
-    let numcrystals = Number(crystals.innerHTML);
-    crystals.innerHTML = numcrystals + 1;
+    popPlus.classList.add('active');
+  })
+
+  document.addEventListener('click', (e) => {
+    const lkEl = e.composedPath().includes(popPlus);
+    const gambLk = e.composedPath().includes(crystalsPlus);
+    const gambLk2 = e.composedPath().includes(coinPlus);
+    if (!lkEl && !gambLk) {
+      if(!gambLk2){
+        popPlus.classList.remove('active');
+      }
+    }
+
+  })
+
+  document.addEventListener('click', (e) => {
+    const lkEl = e.composedPath().includes(popMinus);
+    const gambLk = e.composedPath().includes(crystalsMinus);
+    const gambLk2 = e.composedPath().includes(coinMinus);
+    if (!lkEl && !gambLk) {
+      if(!gambLk2){
+        popMinus.classList.remove('active');
+      }
+    }
+
   })
 
 })();
