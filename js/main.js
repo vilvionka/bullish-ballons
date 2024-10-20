6d2231fc2b8e3ff28a10aeaecb6df1b4d9bb203b
vilivonka <gromootvod@lenta.ru>
2024-10-20 21:48:25 +0300

    last menu

--- js/main.js
+++ js/main.js
@@ -88,3 +88,13 @@
 }
 
 })();
+
+(() => {
+  const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+  }
+  window.addEventListener('resize', appHeight)
+  appHeight()
+
+})();
